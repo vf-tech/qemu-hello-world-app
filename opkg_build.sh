3cab92767976669c99919f5e12f999f3976188e4
Mehmet Alinbay <mehmet.alinbay@pavotek.com.tr>
2022-12-28 18:11:24 +0300

    opkg build SDK'ya gore guncellendi.

--- opkg_build.sh
+++ opkg_build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 PACKAGE=qemu-hello-world
+OPKG_BUILD_PREFIX=/opt/qemu-versatilepb-sdk/bin/opkg-build
 echo Versiyon Numarasi Giriniz:
 read PACKAGE_VER
 echo $PACKAGE_VER
 
 cp build/$PACKAGE opkg-build/root/
 cd opkg-build
-/home/malinbay/workspace/qemu/fw/buildroot-2018.08/output/host/bin/opkg-build .
+$OPKG_BUILD_PREFIX .
 
 exit $?
 
